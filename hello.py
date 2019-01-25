fc7a07d04c49c0c49e9979d062b7cb31ba01c7cd
KirillKlimchuk <Kirill.Klimtchuk@gmail.com>
2019-01-25 06:04:30 +0300

    add gunicorn conf

---
+++ hello.py
@@ -0,0 +1,4 @@
+def application(env, start_response):
+	start_response('200 OK', [('Content-Type', 'text/plain')])
+	body = [bytes(i + '\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
+	return body
