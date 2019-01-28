4a6c86fa913db13581fa77824d758c3e82daae8f
KirillKlimchuk <Kirill.Klimtchuk@gmail.com>
2019-01-28 08:02:28 +0300

    fix

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def application(env, start_response):
 	start_response('200 OK', [('Content-Type', 'text/plain')])
-	body = [bytes(i + '\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
+	body = [(i + '\n') for i in env['QUERY_STRING'].split('&')]
 	return body
